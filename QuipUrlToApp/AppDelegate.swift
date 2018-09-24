8c779e90326a2888553eea66350e170e1e710832
Alasdair Monk <alasdair.monk@gmail.com>
2018-09-24 11:39:01 +0100

    Initial Commit

---
+++ QuipUrlToApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  QuipUrlToApp
+//
+//  Created by Alasdair Lampon-Monk on 24/09/2018.
+//  Copyright © 2018 Alasdair Lampon-Monk. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ QuipUrlToApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  QuipUrlToApp
+//
+//  Created by Alasdair Lampon-Monk on 24/09/2018.
+//  Copyright © 2018 Alasdair Lampon-Monk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
